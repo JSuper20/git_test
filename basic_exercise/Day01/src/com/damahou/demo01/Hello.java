598c4e4627c2dea58ec3024d8228c4c43fa486a0
damahou <194343476@qq.com>
2021-01-06 22:04:30 +0800

    hello.java

---
+++ basic_exercise/Day01/src/com/damahou/demo01/Hello.java
@@ -0,0 +1,10 @@
+package com.damahou.demo01;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+        System.out.println("你好啊");
+    }
+}
